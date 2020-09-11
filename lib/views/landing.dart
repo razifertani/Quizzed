58881587ec04ea311f60548242867e1a729c14ee
Razi <razifertani1@gmail.com>
2020-09-11 23:05:42 +0100

    Application 1.3

--- lib/views/landing.dart
+++ lib/views/landing.dart
@@ -1,3 +1,4 @@
+import 'package:data_connection_checker/data_connection_checker.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:QuizzedGame/views/signin.dart';
@@ -10,10 +11,20 @@ class Landing extends StatefulWidget {
 }
 
 class _LandingState extends State<Landing> {
+  bool isConnected = false;
   @override
   void initState() {
-    startTime();
+    checkConnectivity();
     super.initState();
+  }
+
+  checkConnectivity() async {
+    await DataConnectionChecker().hasConnection.then((value) {
+      setState(() {
+        isConnected = value;
+        isConnected == true ? startTime() : null;
+      });
+    });
   }
 
   startTime() async {
@@ -33,58 +44,144 @@ class _LandingState extends State<Landing> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.4,
-            ),
-            Center(
-              child: RichText(
-                text: TextSpan(
-                  style: TextStyle(
-                    fontSize: MediaQuery.of(context).size.width * 0.13,
+      body: isConnected == true
+          ? Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.4,
                   ),
-                  children: <TextSpan>[
-                    TextSpan(
-                      text: 'Quizzed',
-                      style: TextStyle(
-                        fontFamily: 'Airbnb',
-                        fontWeight: FontWeight.w600,
-                        color: Colors.black54,
+                  Center(
+                    child: RichText(
+                      text: TextSpan(
+                        style: TextStyle(
+                          fontSize: MediaQuery.of(context).size.width * 0.13,
+                        ),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: 'Quizzed',
+                            style: TextStyle(
+                              fontFamily: 'Airbnb',
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black54,
+                            ),
+                          ),
+                          TextSpan(
+                            text: 'Game',
+                            style: TextStyle(
+                              fontFamily: 'Airbnb',
+                              fontWeight: FontWeight.w600,
+                              color: Colors.blue,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                    TextSpan(
-                      text: 'Game',
-                      style: TextStyle(
-                        fontFamily: 'Airbnb',
-                        fontWeight: FontWeight.w600,
-                        color: Colors.blue,
+                  ),
+                  Spacer(),
+                  Text(
+                    'Best quiz game ever',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontFamily: 'Airbnb',
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black54,
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.1,
+                  ),
+                ],
+              ),
+            )
+          : Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.4,
+                  ),
+                  Center(
+                    child: RichText(
+                      text: TextSpan(
+                        style: TextStyle(
+                          fontSize: MediaQuery.of(context).size.width * 0.13,
+                        ),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: 'Quizzed',
+                            style: TextStyle(
+                              fontFamily: 'Airbnb',
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black54,
+                            ),
+                          ),
+                          TextSpan(
+                            text: 'Game',
+                            style: TextStyle(
+                              fontFamily: 'Airbnb',
+                              fontWeight: FontWeight.w600,
+                              color: Colors.blue,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.12,
+                  ),
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.2,
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    child: Column(
+                      children: [
+                        Text(
+                          'No internet connection !',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 25,
+                          ),
+                        ),
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.02,
+                        ),
+                        RaisedButton(
+                          onPressed: () {
+                            checkConnectivity();
+                          },
+                          child: Text(
+                            'TRY AGAIN',
+                            style: TextStyle(
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Spacer(),
+                  Text(
+                    'Best quiz game ever',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontFamily: 'Airbnb',
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black54,
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.1,
+                  ),
+                ],
               ),
             ),
-            Spacer(),
-            Text(
-              'Best quiz game ever',
-              style: TextStyle(
-                fontSize: 15,
-                fontFamily: 'Airbnb',
-                fontWeight: FontWeight.w600,
-                color: Colors.black54,
-              ),
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.1,
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
